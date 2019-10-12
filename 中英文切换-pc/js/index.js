4f0ec791f9649ea52cea9e383db7868b5a7df29f
laixiaoyin <1012551253@qq.com>
2019-10-12 11:30:33 +0800

    new page

---
+++ 中英文切换-pc/js/index.js
@@ -0,0 +1,63 @@
+var index = new Vue({
+	el: "#app",
+	data: function(){
+		return {
+			lang: "ch",
+			content: {}
+		}
+	},
+	mounted: function() {
+		if(localStorage.getItem('save_language')){
+			this.lang = localStorage.getItem('save_language');
+		}else{
+			localStorage.setItem('save_language',this.lang);
+		}
+		this.getData();
+		
+		this.$nextTick(function(){
+			$('.slider-nav').slick({
+				autoplaySpeed: 4000,
+				dots: false,
+				arrows: false,
+				autoplay: true,
+				pauseOnHover: false
+			});
+			$(".banner .slick-dots li").each(function() {
+				var ss = $(this).find("button").html();
+				var txt = "No" + ss;
+				$(this).find("button").html(txt);
+			});
+			$('.ban-news').slick({
+				autoplaySpeed: 4000,
+				slidesToShow: 4,
+				dots: false,
+				arrows: true,
+				autoplay: true,
+				pauseOnHover: false
+			});
+		});
+	},
+	methods: {
+		changeLanguage: function (index){
+			var list = ['ch','en'];
+			this.lang = list[index];
+			this.getData();
+			localStorage.setItem('save_language',this.lang);
+		},
+		getData: function (){
+			var that = this;
+			var url = "language/index/index_ch.json";
+			if(this.lang == "en"){
+				url = "language/index/index_en.json";
+			}
+			$.ajax({
+				url: url,
+				type: "GET",
+				async: false,
+				success: function(res){
+					that.content = res;
+				}
+			});
+		}
+	}
+});
---
+++ 中英文切换-pc/js/team.js
@@ -0,0 +1,52 @@
+var index = new Vue({
+	el: "#app",
+	data: function(){
+		return {
+			lang: "ch",
+			content: {}
+		}
+	},
+	mounted: function() {
+		if(localStorage.getItem('save_language')){
+			this.lang = localStorage.getItem('save_language');
+		}else{
+			localStorage.setItem('save_language',this.lang);
+		}
+		this.getData();
+		
+		this.$nextTick(function(){
+			
+			$('.m-members .slider').slick({
+				dots: false,
+				arrows: true,
+				autoplay: false,
+				slidesToShow: 4,
+			});
+		});
+	},
+	methods: {
+		// 切换语言
+		changeLanguage: function (index){
+			var list = ['ch','en'];
+			this.lang = list[index];
+			this.getData();
+			localStorage.setItem('save_language',this.lang);
+		},
+		// 获取语言包
+		getData: function (){
+			var that = this;
+			var url = "language/team/team_ch.json";
+			if(this.lang == "en"){
+				url = "language/team/team_en.json";
+			}
+			$.ajax({
+				url: url,
+				type: "GET",
+				async: false,
+				success: function(res){
+					that.content = res;
+				}
+			});
+		}
+	}
+});
